539a48f70647693cd74d71095b70827594f17533
rasikkumar87 <52450192+rasikkumar87@users.noreply.github.com>
2020-03-28 15:25:49 +0000

    Update trouva_cleanse_demo.py
    Script update

--- size-sanitizer/trouva_cleanse_demo.py
+++ size-sanitizer/trouva_cleanse_demo.py
@@ -7,3 +7,14 @@ Created on Wed Mar 25 12:55:16 2020
 
 #Primarily using the pandas library for this cleansing demo
 import pandas as pd
+
+#First, reading the JSON file generated from the Mongo Seed (this was done with an external file modification to Parse JSON)
+#Storing this to a Pandas Dataframe
+df = pd.read_json(r'sizes_json.json')
+
+#Getting a list of all the labels in that column
+all_labels = df['label'].tolist()
+
+#Creating new columns to store cleaned data demoed in this script
+df['cleanItemDescription'] = ''
+df['cleanItemSize'] = ''
